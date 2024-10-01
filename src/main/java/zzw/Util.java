2ca39391f065c5c0d6a74710433887467cc4520e
ld <zzw422339@163.com>
2024-10-01 17:58:47 +0800

    ForkJoinPool 归并排序 VS 快速排序

--- src/main/java/zzw/Util.java
+++ src/main/java/zzw/Util.java
@@ -1,14 +1,45 @@
 package zzw;
 
-public class Util {
+import java.util.Random;
 
-    private Util() {
+public class Util
+{
+
+    private Util()
+    {
     }
 
-    public static void sleep(long millis) {
-        try {
+    public static void sleep(long millis)
+    {
+        try
+        {
             Thread.sleep(millis);
-        } catch (InterruptedException ignore) {
+        }
+        catch (InterruptedException ignore)
+        {
+        }
+    }
+
+    private static final Random RANDOM = new Random();
+
+    /**
+     * 生成一个长度为 length 的随机数组, 每个数字的范围为 [0, bound]
+     */
+    public static int[] generateRandomArray(int length, int bound)
+    {
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            arr[i] = RANDOM.nextInt(bound + 1);
+        }
+        return arr;
+    }
+
+    public static void isSorted(int[] arr)
+    {
+        for (int i = 0; i + 1 < arr.length; i++)
+        {
+            if (arr[i] > arr[i + 1]) throw new RuntimeException();
         }
     }
 }
