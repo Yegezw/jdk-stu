999b7402a91c85655b01e61b08f63e3bd0edd2d7
ld <zzw422339@163.com>
2024-10-04 15:42:12 +0800

    更新多线程题目代码

--- src/main/java/zzw/problem/Test3.java
+++ src/main/java/zzw/problem/Test3.java
@@ -3,54 +3,66 @@ package zzw.problem;
 /**
  * 三个线程分别打印 1 2 3, 顺序执行 10 次
  */
+@SuppressWarnings("all")
 public class Test3
 {
 
     /**
      * 线程互斥的关键
      */
-    private static volatile int     t      = 0;
-    private static          int     count  = 1;
+    private static volatile int     point;
+    /**
+     * 已打印的轮数
+     */
+    private static          int     count  = 0;
+    /**
+     * 任务是否已完成
+     */
     private static          boolean finish = false;
 
-    /**
-     * 当 t == flag 时打印 i
-     */
-    private static void print(final int flag, final int i)
+    private static class OrderedThread extends Thread
     {
-        while (!finish)
-        {
-            if (t == flag)       // ------ 加锁 ------
-            {
-                if (count > 10)
-                {
-                    finish = true;
-                    break;
-                }
+        private final int id;
+        private final int num;
 
-                System.out.println(Thread.currentThread().getName() + i);
-                if (i == 3)
+        public OrderedThread(int id, int num)
+        {
+            super("线程 " + id + " ");
+            this.id  = id;
+            this.num = num;
+        }
+
+        @Override
+        public void run()
+        {
+            while (!finish)
+            {
+                if (point == id)       // ------ 加锁 ------
                 {
-                    count++;
+                    if (count >= 10)
+                    {
+                        finish = true;
+                        return;
+                    }
+
+                    System.out.println(super.getName() + num);
+                    if (num == 3) count++;
+
+                    if (point == 3) point = 1;
+                    else point++;      // ------ 解锁 ------
                 }
-                t = (t + 1) % 3; // ------ 解锁 ------
             }
         }
     }
 
     public static void main(String[] args)
     {
-        Thread t1 = new Thread(() -> print(0, 1));
-        t1.setName("线程 A ");
-
-        Thread t2 = new Thread(() -> print(1, 2));
-        t2.setName("线程 B ");
-
-        Thread t3 = new Thread(() -> print(2, 3));
-        t3.setName("线程 C ");
-
-        t1.start();
-        t2.start();
-        t3.start();
+        point = 1;
+        OrderedThread thread1 = new OrderedThread(1, 1);
+        OrderedThread thread2 = new OrderedThread(2, 2);
+        OrderedThread thread3 = new OrderedThread(3, 3);
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }
