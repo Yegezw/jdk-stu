0b039a6ee212d2a0ab0e466df738cb223a22c566
ld <zzw422339@163.com>
2024-10-11 00:31:35 +0800

    执行链 + 执行组

---
+++ src/main/java/zzw/chain/RunnableChain.java
@@ -0,0 +1,59 @@
+package zzw.chain;
+
+import java.util.LinkedList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * 执行链
+ */
+public class RunnableChain
+{
+
+    private final String                           chainName;
+    private final ExecutorService                  executorService;
+    private final LinkedList<RunnableGroup>        groups;
+    private final ConcurrentLinkedQueue<Throwable> throwableQueue;
+
+    private RunnableChain(final String chainName, final ExecutorService executorService)
+    {
+        this.chainName       = chainName;
+        this.executorService = executorService;
+        this.groups          = new LinkedList<>();
+        this.throwableQueue  = new ConcurrentLinkedQueue<>();
+    }
+
+    public static RunnableChain create(final String chainName, final ExecutorService executorService)
+    {
+        return new RunnableChain(chainName, executorService);
+    }
+
+    // =================================================================================================================
+
+    public RunnableChain with(final String groupName, final Runnable... runnable)
+    {
+        groups.add(RunnableGroup.create(chainName, groupName, executorService, throwableQueue, runnable));
+        return this;
+    }
+
+    /**
+     * 阻塞方法
+     */
+    public boolean start()
+    {
+        for (RunnableGroup group : groups)
+        {
+            if (!group.startAndAwait())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @SuppressWarnings("all")
+    public ConcurrentLinkedQueue<Throwable> getThrowableQueue()
+    {
+        return throwableQueue;
+    }
+}
